159e7b429dffbae04346fa76ab3147dfb01066de
shashidhar mandalozu <shajhansi@shashidhars-MacBook-Pro.local>
2020-12-08 21:21:13 +0000

    Searchbar & Countries Profiling Functionality Done

--- js/control.js
+++ js/control.js
@@ -1,3 +1,60 @@
 //controls
 L.control.scale().addTo(map);
 map.zoomControl.setPosition('bottomright');
+
+
+// Search bar on the map 
+  let controlSearch = new L.Control.Search({
+    position:'topright',    // Location of the search bar?
+    layer: district_boundary,  // name of the layer
+    initial: false,
+		marker: false,
+    textPlaceholder: 'Search...',
+	propertyName : 'name'
+});
+  
+    controlSearch.on('search:locationfound', function(e) {
+      
+	 district_boundary.eachLayer(function(layer) {
+		
+    if (layer.feature.properties.name == e.text) {
+		highlight_boundary.clearLayers();
+		highlight_boundary.addData(layer.feature);
+        map.fitBounds(layer.getBounds());
+		highlight_boundary.setStyle(highstyle);
+		LoadCountryInfo(e.text	)
+    }
+})
+  })
+ 
+map.addControl(controlSearch); // add it to the map
+
+// Loading Country's Information
+function LoadCountryInfo(name){
+	//var countryHtml = "<h3 class = 'm-2 text-primary text-center'>"+ name +"</h3>";
+	
+	
+	
+	$.ajax({
+               type:"GET",
+               dataType: "json",
+               url:"https://restcountries.eu/rest/v2/name/"+name+"?fullText=true",
+               success:function(data)
+               {
+				   console.log(data);
+				var countryHtml = "<div class='card text-white bg-info m-3' style='max-width: 30vw'><div class='card-header'><h1>"+name+"</h1></div><div class='card-body'><p class='card-text'>";  
+				  countryHtml += "<table class='table table-borderless text-white'>";  
+                  countryHtml += "<tr><th>Capital</th><td>"+data[0].capital+"</td></tr>";  
+                  countryHtml += "<tr><th>Population</th><td>"+data[0].population+"</td></tr>";  
+                  countryHtml += "<tr><th>Flag</th><td><img src="+data[0].flag+" style='height:80px'></td></tr>";
+				  countryHtml += "<tr><th>Currency</th><td>"+data[0].currencies[0].name+"</td></tr>";  
+                  countryHtml += "</table></p></div></div>";  
+				  $("#info_div").html(countryHtml);
+                
+               }
+            });
+	
+	
+}
+ 
+ 
--- js/loadMap.js
+++ js/loadMap.js
@@ -1,40 +1,71 @@
+//Adding map
+const map = L.map("map", { attributionControl: false }).setView([0, 0], 1.5);
 
-//adding map and base layer
-const map = L.map('map',{attributionControl: false}).setView([0,0], 1.4);
-
+// Adding base layer
 const layer = new L.StamenTileLayer("toner");
 map.addLayer(layer);
 
-let countries_tab = "<table class='table table-hover mt-2' style='font-family:Comic Sans,arial,helvetica;'> <thead><tr><th scope='col' class='text-warning'>Select A Country</th></tr></thead><tbody>";
+// Pre-defined Countries List Table
+let countries_tab = `
+        <table class='table table-hover' style='font-family:arial, helvetica;'>
+        <thead>
+        <tr><th scope='col' class='text-info' style='font-size: 20px'>Select Any Country</th></tr>
+        </thead>
+        <tbody>`;
+
+//
 const district_boundary = new L.geoJson();
 district_boundary.addTo(map);
 
+//
 $.ajax({
-dataType: "json",
-url: "data/countryBorders.geo.json",
-success: function(data) {
-    $(data.features).each(function(key, data) {
-        district_boundary.addData(data);
-		
-		countries_tab += "<tr><td>"+ data.properties.name +"</td></tr>"
+  dataType: "json",
+  url: "data/countryBorders.geo.json",
+  success: function (data) {
+    $(data.features).each(function (key, data) {
+      district_boundary.addData(data);
+      countries_tab +=
+        "<tr><td onClick=zoomTo(this)>" + data.properties.name + "</td></tr>";
     });
-	
-	  countries_tab += "</tbody></table>";
-	  $('#country_div').html(countries_tab);
-	  district_boundary.setStyle(polystyle);
-}
-}).error(function() {});
+
+    countries_tab += "</tbody></table>";
+    $("#country_div").html(countries_tab);
+    district_boundary.setStyle(polystyle);
+  },
+}).error(function () {});
 
 function polystyle(feature) {
-    return {
-        fillColor: 'green',
-        weight: 0.1,
-        opacity: 0.1,
-        color: 'white',  //Outline color
-        fillOpacity: 0.1
-    };
+  return {
+    fillColor: "green",
+    weight: 0.1,
+    opacity: 0.1,
+    color: "white", //Outline color
+    fillOpacity: 0.7,
+  };
 }
 
+//
+const highlight_boundary = new L.geoJson();
+highlight_boundary.addTo(map);
 
-
-
+function highstyle(feature) {
+  return {
+    fillColor: "brown",
+    weight: 0.1,
+    opacity: 0.1,
+    color: "white", //Outline color
+    fillOpacity: 0.7,
+  };
+}
+function zoomTo(e) {
+  country = $(e).html();
+  district_boundary.eachLayer(function (layer) {
+    if (layer.feature.properties.name == country) {
+      highlight_boundary.clearLayers();
+      highlight_boundary.addData(layer.feature);
+      map.fitBounds(layer.getBounds());
+      highlight_boundary.setStyle(highstyle);
+      LoadCountryInfo(country);
+    }
+  });
+}
